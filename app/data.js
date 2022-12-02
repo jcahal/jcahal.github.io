2c3986fd366b973bf4efc7409f501a90ea8eb0a6
jcahal <jacahal@gmail.com>
2022-12-02 11:45:50 -0700

    firebase and admin removal

---
+++ app/data.js
@@ -0,0 +1,49 @@
+var Data = {
+  "projects": {
+    "ANSR%20Ascend%20Project": {
+      "description": "NASA funded high altitude balloon experiment. 2015-2016 school year firmware team member.",
+      "image": "G0010326.JPG",
+      "name": "ANSR Ascend Project",
+      "source": "https://github.com/PC-Ascend-Team",
+      "url": "http://pc-ascend-team.github.io"
+    },
+    "Gnito": {
+      "description": "Self-destructing one-way messenger",
+      "image": "gnito.png",
+      "name": "Gnito",
+      "source": "https://github.com/jcahal/gnito",
+      "url": "https://jcahal.github.io/gnito-client/"
+    },
+    "StreetCard%20-%20SHUT%20DOWN": {
+      "description": "Connects homeless people with services and healthcare, easily and securely.",
+      "image": "VectorGenStreetCard.png",
+      "name": "StreetCard"
+    }
+  },
+  "skills": {
+    "C%2FC%2B%2B": {
+      "level": 5,
+      "name": "C/C++"
+    },
+    "Communication": {
+      "level": "5",
+      "name": "Communication"
+    },
+    "Embedded%20Linux": {
+      "level": "4",
+      "name": "Embedded Linux"
+    },
+    "JS%2FHTML%2FCSSS": {
+      "level": "5",
+      "name": "JS/HTML/CSSS"
+    },
+    "Leadership": {
+      "level": "4",
+      "name": "Leadership"
+    },
+    "Multitasking": {
+      "level": "4",
+      "name": "Multitasking"
+    }
+  }
+}
