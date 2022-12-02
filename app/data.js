c792975bd39afc507217ef140e19f00f38dc21c7
jcahal <jacahal@gmail.com>
2022-12-02 12:18:07 -0700

    project image rework

--- app/data.js
+++ app/data.js
@@ -2,21 +2,24 @@ var Data = {
   "projects": {
     "ANSR%20Ascend%20Project": {
       "description": "NASA funded high altitude balloon experiment. 2015-2016 school year firmware team member.",
-      "image": "G0010326.JPG",
+      "image": "https://jcahal.github.io/assets/images/AscendProject.png",
+      "image-alt-text": "Image of Ascend project payloads at near space altitude.",
       "name": "ANSR Ascend Project",
       "source": "https://github.com/PC-Ascend-Team",
       "url": "http://pc-ascend-team.github.io"
     },
     "Gnito": {
       "description": "Self-destructing one-way messenger",
-      "image": "gnito.png",
+      "image": "https://jcahal.github.io/assets/images/GnitoProject.png",
+      "image-alt-text": "Image of the Gnito project homepage.",
       "name": "Gnito",
       "source": "https://github.com/jcahal/gnito",
       "url": "https://jcahal.github.io/gnito-client/"
     },
     "StreetCard%20-%20SHUT%20DOWN": {
       "description": "Connects homeless people with services and healthcare, easily and securely.",
-      "image": "VectorGenStreetCard.png",
+      "image": "https://jcahal.github.io/assets/images/StreetCardProject.png",
+      "image-alt-text": "Image of slide from StreetCard Project Presentation.",
       "name": "StreetCard"
     }
   },
